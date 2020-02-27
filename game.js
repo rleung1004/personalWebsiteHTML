e9b3f51d108f8bc0fca1ed3ff7bfc4fc5ae73b76
rleung1004 <59620700+rleung1004@users.noreply.github.com>
2020-02-27 12:50:46 -0800

    added new image sprites

--- game.js
+++ game.js
@@ -1,30 +1,82 @@
 const gameContainer = document.getElementById("gameContainer");
 const menu = document.getElementById("menu");
-const zombies = document.querySelectorAll(".zombie");
+const scoreBoard = document.getElementById("score");
+const timeCount = document.getElementById("time");
+const endButton = document.getElementById("end");
 
 function spawnZombie() {
     let zombie = document.createElement("img");
-    zombie.className = "zombie";
+    zombie.className = "zombie alive";
     zombie.src = "images/zombie-cet.png";
-    zombie.style = "width: 100px; height: 100px";
     zombie.style.left = 0;
     zombie.style.top = parseInt(Math.random() * 80) + "%";
     gameContainer.appendChild(zombie);
-    moveZombie(zombie);
-}
 
-function moveZombie(zombie) {
-    let moving = true;
     let pos = 0;
     let id = setInterval(frame, 5);
     function frame() {
         if (pos.toFixed() === "93") {
             clearInterval(id);
+            endGame();
         } else {
             pos += 0.1;
             zombie.style.left = pos + "%";
         }
     }
+
+    zombie.onclick = function e() {
+        clearInterval(id);
+        killZombie(zombie);
+        this.onclick = null;
+    }
 }
 
-gameContainer.onclick = spawnZombie;
+function killZombie(zombie) {
+    zombie.src = "images/ded-cet.png";
+    zombie.className = "zombie dead";
+    updateScore();
+}
+
+function updateTime() {
+    window.timer--;
+    timeCount.innerHTML = "Time: " + timer;
+    if (time === 0) {
+        endGame();
+    }
+}
+
+function updateScore() {
+    window.score++;
+    scoreBoard.innerHTML = "Score: " + window.score;
+}
+
+function clearDeadZombies() {
+    let deadZombies = document.querySelectorAll(".dead");
+    deadZombies.forEach(function removeNode(node){
+        node.remove();
+    });
+}
+
+function updateSpawnRate() {
+    spawnRate -= 600;
+    clearInterval(window.runGame);
+    window.runGame = setInterval(spawnZombie, spawnRate);
+}
+
+function startGame() {
+    window.timer = 100;
+    window.score = 0;
+    window.spawnRate = 3000;
+    window.runGame = setInterval(spawnZombie, spawnRate);
+    window.time = setInterval(updateTime, 1500);
+    window.cleanUp = setInterval(clearDeadZombies, 10000);
+    window.updateSpawn = setInterval(updateSpawnRate, 30000);
+    gameContainer.style.background = "images/spookyBackground.png";
+}
+
+function endGame() {
+    clearInterval(runGame);
+    clearInterval(time);
+    clearInterval(updateSpawn);
+    clearInterval(cleanUp);
+}
