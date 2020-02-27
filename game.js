832785788fdfea92ff0b47224837368d9488e7dd
rleung1004 <59620700+rleung1004@users.noreply.github.com>
2020-02-27 01:15:20 -0800

    added animation to zombies

--- game.js
+++ game.js
@@ -1,13 +1,30 @@
-let container = document.getElementById("container");
+const gameContainer = document.getElementById("gameContainer");
+const menu = document.getElementById("menu");
+const zombies = document.querySelectorAll(".zombie");
 
 function spawnZombie() {
     let zombie = document.createElement("img");
     zombie.className = "zombie";
-    zombie.src = "images/zombie-cet.png"
+    zombie.src = "images/zombie-cet.png";
     zombie.style = "width: 100px; height: 100px";
     zombie.style.left = 0;
     zombie.style.top = parseInt(Math.random() * 80) + "%";
-    container.appendChild(zombie);
+    gameContainer.appendChild(zombie);
+    moveZombie(zombie);
 }
 
-container.onclick = spawnZombie;
+function moveZombie(zombie) {
+    let moving = true;
+    let pos = 0;
+    let id = setInterval(frame, 5);
+    function frame() {
+        if (pos.toFixed() === "93") {
+            clearInterval(id);
+        } else {
+            pos += 0.1;
+            zombie.style.left = pos + "%";
+        }
+    }
+}
+
+gameContainer.onclick = spawnZombie;
